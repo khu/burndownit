649e2eca97da617d4fd00b04f4935d08e3436bb1
hukai <khu@thoughtworks.com>
2011-02-28 04:05:09 +1100

    added everything into a

---
+++ public/js/burndown.js
@@ -0,0 +1,149 @@
+Array.prototype.avg = function() {
+	var av = 0;
+	var cnt = 0;
+	var len = this.length;
+	for (var i = 0; i < len; i++) {
+	var e = +this[i];
+	if(!e && this[i] !== 0 && this[i] !== '0') e--;
+	if (this[i] == e) {av += e; cnt++;}
+	}
+	return av/cnt;
+}
+
+Array.prototype.sum = function() {
+	var result = 0;
+	var len = this.length;
+	for (var i = 0; i < len; i++) {
+		result += this[i];
+	}
+	return result;
+}
+
+
+function accumulate(arry) {
+	var accumulated = [];
+	for(var i = 0; i < arry.length; i++) {
+		if (i == 0) {
+			accumulated[0] = arry[0]
+		} else {
+			accumulated[i] = arry[i] + accumulated[i-1]
+		}
+	}
+	return accumulated
+}
+
+function predict_velocity(finished, length) {
+	var predict = [];
+	for(var i = 0; i < length; i++) {
+		if (i < finished.length) {
+ 			predict[i] = finished[i];
+		} else {
+			predict[i] = finished[finished.length - 1]
+		}
+	}
+	return predict	
+}
+
+
+var chart;
+jQuery(document).ready(function() {
+
+	var data;
+	var holder = document.getElementById('data');
+	holder.ondragover = function () { this.className = 'hover'; return false; };
+	holder.ondragend = function () { this.className = ''; return false; };
+	holder.ondrop = function (e) {
+	  this.className = '';
+	  e.preventDefault();
+
+	  var file = e.dataTransfer.files[0],
+	  reader = new FileReader();
+	  reader.onload = function (event) {
+			data = event.target.result;
+			eval(data)
+			var accumulated_finished = accumulate(finished);
+			var predict = accumulate(predict_velocity(finished, iterations.length))
+			burndown(planed, finished, scope, accumulated_finished, predict, iterations, title)
+	  };
+	  reader.readAsText(file);
+	  return false;
+	};
+		
+
+});
+
+function burndown(planed, finished, scope, accumulated_finished, predict, iterations, title) {
+	new Highcharts.Chart({
+		chart: {
+			renderTo: 'container'
+		},
+		title: {
+			text: title
+		},
+		xAxis: {
+			categories: iterations
+		},
+		tooltip: {
+			formatter: function() {
+				return  this.x  +': '+ this.y;
+			}
+		},
+		credits: {
+			enabled:true,
+			text:'',
+			href:""
+		},
+		yAxis: {
+			title: {
+				text: 'Story Point'
+			}
+		},
+		plotOptions: {
+			line: {
+				dataLabels: {
+					enabled: true
+				},
+				enableMouseTracking: false
+			}
+		},
+		labels: {
+			items: [{
+				html: 'Finished ' + finished.sum() + ' points in total',
+				style: {
+					left: '40px',
+					top: '8px',
+					color: 'black'				
+				}
+			}]
+		},
+		series: [{
+			type: 'column',
+			name: 'Planed',
+			color:'green',
+			data: planed
+		}, {
+			type: 'column',
+			name: 'Finished',
+			color:'#00B2EE',
+			data: finished,
+		}, {
+			type: 'line',
+			name: 'Scope',
+			color:'#FF8C00',
+			data: scope
+		}, {
+				type: 'line',
+				name: 'Prediction',
+				dashStyle: 'longdash',
+				color: '#AAAAAA',
+				data: predict
+			},
+		{
+			type: 'line',
+			color: 'black',
+			name: 'Finished',
+			data: accumulated_finished
+		}]
+	});
+}
+
